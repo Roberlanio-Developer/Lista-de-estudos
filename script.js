f705e5e3e0e52ff1186aaa6fe01ac3f2d8b49fc8
Roberlanio-Developer <fabiano.eng@hotmail.com>
2022-08-29 23:01:55 -0300

    Lista de tarefa 80% concluida

--- script.js
+++ script.js
@@ -1,37 +1,53 @@
+const inputmateria = document.getElementById('Materia-Nova');
+let itemId = 1;
+const lista = document.querySelector('#lista');
 
+function removeItem(id) {
+  /**
+   * Aqui eu seleciono o LI que foi criado na linha 12
+   * Usei parentNode porque vc criou um li dentro do outro para cada item, e eu preciso remover um filho da lista(UL)
+   * Exemplo: 
+   *            <li>
+   *                <li>
+   *                   <span>${inputmateria.value}</span>
+                       <button class="excluir">X</button>
+   *                </li>
+   *            </li>
+   *  */
+  const itemToRemove = document.getElementById(id).parentNode;
 
-
-const inputmateria = document.
-getElementById("Materia-Nova")
-
-function adicionarmateria () {
-    const item = document.createElement("li");
-
-    item.innerHTML = `<li>
-    <span>${inputmateria.value}</span>
-    <button class="excluir">X</button>
-</li>`;
-
-
-    const lista = document.querySelector("#lista")
-
-    lista.appendChild(item)
-
-    inputmateria.value=""
-
+  /**
+   * a função removeChild recebe o Elemento e o remove da DOM
+   *  */
+  lista.removeChild(itemToRemove);
 }
 
-const botao = document.querySelector("#btn-adicionar")
-botao.addEventListener("click", adicionarmateria);
+function adicionarmateria() {
+  const item = document.createElement('li');
 
-function limparlista(){
-    
-    const lista = document.querySelector("#lista"); lista.innerHTML=""
+  item.innerHTML = `<li id=${itemId}>
+                      <span>${inputmateria.value}</span>
+                      <button onClick={removeItem(${itemId})} class="excluir">X</button>
+                   </li>`;
 
+  lista.appendChild(item);
+
+  inputmateria.value = '';
+  itemId++;
 }
 
-const botaolimpar = document.querySelector(".bt-azul")
-botaolimpar.addEventListener("click", limparlista);
+const botao = document.querySelector('#btn-adicionar');
+botao.addEventListener('click', adicionarmateria);
+
+function limparlista() {
+  lista.innerHTML = '';
+}
+
+const botaolimpar = document.querySelector('.bt-azul');
+botaolimpar.addEventListener('click', limparlista);
+
+
+
 
 
 
